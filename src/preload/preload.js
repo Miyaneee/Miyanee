942301f6a5818fcad54b21d3fc84ec7cafeb8f6b
Gu-Miao <1450206741@qq.com>
2022-04-19 17:48:35 +0800

    Export ipcrenderer functions

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,6 +1,39 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { version } from '@@/package.json'
 
 contextBridge.exposeInMainWorld('miyanee', {
-  version
+  version,
+  renderer: {
+    /**
+     * Send to main process
+     * @param {string} channel
+     * @param {*} data
+     */
+    send(channel, data) {
+      ipcRenderer.send(channel, data)
+    },
+    /**
+     * Watch channel
+     * @param {string} channel
+     * @param {function} callback
+     */
+    on(channel, callback) {
+      ipcRenderer.on(channel, callback)
+    },
+    /**
+     * Watch channel once
+     * @param {string} channel
+     * @param {function} callback
+     */
+    once(channel, callback) {
+      ipcRenderer.once(channel, callback)
+    },
+    /**
+     * Remove all listeners of a channel
+     * @param {string} channel
+     */
+    removeAllListeneers(channel) {
+      ipcRenderer.removeAllListeners(channel)
+    }
+  }
 })
