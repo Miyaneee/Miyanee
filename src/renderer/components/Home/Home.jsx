67394bdff0cbb04921912247f4d0b0d034b15c04
Gu-Miao <1450206741@qq.com>
2022-05-06 14:51:59 +0800

    Update Home component
    1. Split list components
    2. Update styles
    3. Add download handler

--- src/renderer/components/Home/Home.jsx
+++ src/renderer/components/Home/Home.jsx
@@ -1,14 +1,15 @@
 import { useState, memo } from 'react'
-import { InputGroup, Input, Button } from 'rsuite'
-import { Search } from '@rsuite/icons'
-import { selectPlugins } from '@/store/plugins'
+import { InputGroup, Input, Whisper, Popover, Button } from 'rsuite'
+import { Search, CheckRound } from '@rsuite/icons'
+import { selectApps, startDownload, handleDownloadFail, handledownloadSuccess } from '@/store/apps'
 import { selectLayout, showPage } from '@/store/layout'
 import { useSelector, useDispatch } from 'react-redux'
+import cls from 'classnames'
 import { request, downloadApp } from '@/utils'
 import './Home.less'
 
 function Home({ show }) {
-  const plugins = useSelector(selectPlugins)
+  const apps = useSelector(selectApps)
   const layout = useSelector(selectLayout)
   const dispatch = useDispatch()
   const [value, setValue] = useState('')
@@ -18,12 +19,13 @@ function Home({ show }) {
   async function search() {
     setSearching(true)
     const [err, res] = await request(
-      `https://registry.npmjs.com/-/v1/search?from=0&text=keywords:vite is:unstable not:unstable ${value}&quality=0.5&popularity=1.0&maintenance=0.1`
+      `https://registry.npmjs.com/-/v1/search?from=0&text=keywords:miyanee-app is:unstable not:unstable ${value}&quality=0.5&popularity=1.0&maintenance=0.1`
     )
     if (err) {
       console.log(err)
       return
     }
+    console.log(res.data)
     setResults(res.data.objects)
   }
   function handleChange(value) {
@@ -32,7 +34,7 @@ function Home({ show }) {
     }
     setValue(value)
   }
-  function openPlugin(plugin) {
+  function openApp(plugin) {
     const opened = layout.pages.find(page => page.label === plugin.name)
     if (opened) {
       dispatch(showPage(opened.label))
@@ -46,39 +48,108 @@ function Home({ show }) {
     }
   }
   return (
-    <div
-      className="Home"
-      style={{
-        display: show ? '' : 'none'
-      }}
-    >
+    <div className={cls('Home', { hide: !show })}>
       <InputGroup inside className="search">
         <Input value={value} onChange={handleChange} />
         <InputGroup.Button onClick={search}>
           <Search />
         </InputGroup.Button>
       </InputGroup>
-
-      {!searching && (
-        <ul className="plugins">
-          {plugins.map(plugin => (
-            <li key={plugin.name}>
-              <Button onClick={() => openPlugin(plugin)}>{plugin.name}</Button>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {searching && (
-        <ul className="results">
-          {results.map((result, i) => (
-            <li key={i} onClick={() => downloadApp(result)}>
-              <span>{JSON.stringify(result)}</span>
-            </li>
-          ))}
-        </ul>
-      )}
+      {!searching && <AppList apps={apps} onClick={openApp} />}
+      {searching && <ResultList results={results} onOpen={openApp} />}
     </div>
+  )
+}
+
+const AppList = memo(({ apps, onClick }) => {
+  return (
+    <ul className="apps">
+      {apps.map((app, i) => (
+        <li key={i} onClick={() => onClick(app)}>
+          <div className="title">
+            <span>{app.name}</span>
+            {app.isOffical && <OfficalMark />}
+          </div>
+          <div className="desc">{app.description}</div>
+        </li>
+      ))}
+    </ul>
+  )
+})
+
+const ResultList = memo(({ results, onOpen }) => {
+  const apps = useSelector(selectApps)
+  console.log(apps)
+  const dispatch = useDispatch()
+  async function download(object) {
+    const packageName = object.package.name
+    dispatch(
+      startDownload({
+        packageName,
+        ready: false
+      })
+    )
+    const data = await downloadApp(object)
+    console.log('data', data)
+    if (!data) {
+      dispatch(handleDownloadFail(packageName))
+      return
+    }
+    dispatch(handledownloadSuccess(data))
+  }
+  return (
+    <ul className="results">
+      {results.map((result, i) => {
+        const { name, description, scope, version, keywords = [] } = result.package
+        const isOffical = 'miyaneee' === scope
+        const app = apps.find(app => app.packageName === name)
+        const shouldDownload = !app
+        const downloading = app && !app.ready
+        const available = app && app.ready
+        return (
+          <li key={i}>
+            <div className="title">
+              <span>{name}</span>
+              {isOffical && <OfficalMark />}
+            </div>
+            <div className="desc">{description}</div>
+            <div className="meta">
+              {keywords.map(keyword => (
+                <div key={keyword} className="tag">
+                  {keyword}
+                </div>
+              ))}
+            </div>
+            <div className="version">{version}</div>
+            <div className="operation">
+              {shouldDownload && (
+                <Button appearance="primary" onClick={() => download(result)}>
+                  下载
+                </Button>
+              )}
+              {downloading && (
+                <Button appearance="primary" disabled>
+                  下载中...
+                </Button>
+              )}
+              {available && (
+                <Button appearance="primary" color="green" onClick={() => onOpen(app)}>
+                  打开
+                </Button>
+              )}
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
+})
+
+function OfficalMark() {
+  return (
+    <Whisper trigger="hover" speaker={<Popover>Offical Plugin</Popover>}>
+      <CheckRound color="#4cd137" />
+    </Whisper>
   )
 }
 
