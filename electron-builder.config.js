c804b0bcf6f2005430d0717c5e8399472b51e0e8
Gu-Miao <1450206741@qq.com>
2022-07-25 11:42:23 +0800

    Remove notarize

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,43 +1,13 @@
 const { version } = require('electron/package.json')
 
 module.exports = {
-  productName: 'Miyaneee',
-  appId: 'org.miyaneee.Miyaneee',
+  productName: 'Miyanee',
+  appId: 'com.electron.Miyanee',
   asar: true,
   files: ['dist/build', 'resources', 'package.json'],
   removePackageScripts: true,
-  afterSign: 'scripts/notarize.js',
-  mac: {
-    target: {
-      target: 'default',
-      arch: ['arm64', 'x64']
-    },
-    type: 'distribution',
-    hardenedRuntime: true,
-    entitlements: 'resources/entitlements.mac.plist',
-    entitlementsInherit: 'resources/entitlements.mac.plist',
-    gatekeeperAssess: false
-  },
-  dmg: {
-    contents: [
-      {
-        x: 130,
-        y: 220
-      },
-      {
-        x: 410,
-        y: 220,
-        type: 'link',
-        path: '/Applications'
-      }
-    ]
-  },
   win: {
     target: ['nsis']
-  },
-  linux: {
-    target: ['AppImage'],
-    category: 'Development'
   },
   directories: {
     buildResources: 'resources',
