7ceff70a6bea7466e69213bfbd7a7cc07320db0b
Gu-Miao <1450206741@qq.com>
2022-07-26 16:43:45 +0800

    Update Home page

--- src/renderer/features/Home/Home.tsx
+++ src/renderer/features/Home/Home.tsx
@@ -1,9 +1,121 @@
+import Button from '@/components/Button/Button'
+import './Home.less'
+
 function Home() {
   return (
-    <div>
+    <div className="Home">
       <input type="text" />
       <ul>
-        <li></li>
+        <li>
+          <div className="content">
+            <div className="img-wrapper">
+              <img
+                src="https://lf-cdn-tos.bytescm.com/obj/static/xitu_extension/static/github.46c47564.png"
+                alt="img"
+              />
+            </div>
+            <div>
+              <h2>Souls Planner</h2>
+              <p>
+                Dark Souls character planner and calculators.Dark Souls character planner and
+                calculators.Dark Souls character planner and calculators.Dark Souls character
+                planner and calculators.
+              </p>
+            </div>
+          </div>
+          <div className="footer">
+            <Button className="primary">打开</Button>
+            <Button className="danger">删除</Button>
+          </div>
+        </li>
+        <li>
+          <div className="content">
+            <div className="img-wrapper">
+              <img
+                src="https://lf-cdn-tos.bytescm.com/obj/static/xitu_extension/static/github.46c47564.png"
+                alt="img"
+              />
+            </div>
+            <div>
+              <h2>Souls Planner</h2>
+              <p>
+                Dark Souls character planner and calculators.Dark Souls character planner and
+                calculators.Dark Souls character planner and calculators.Dark Souls character
+                planner and calculators.
+              </p>
+            </div>
+          </div>
+          <div className="footer">
+            <Button className="primary">打开</Button>
+            <Button className="danger">删除</Button>
+          </div>
+        </li>
+        <li>
+          <div className="content">
+            <div className="img-wrapper">
+              <img
+                src="https://lf-cdn-tos.bytescm.com/obj/static/xitu_extension/static/github.46c47564.png"
+                alt="img"
+              />
+            </div>
+            <div>
+              <h2>Souls Planner</h2>
+              <p>
+                Dark Souls character planner and calculators.Dark Souls character planner and
+                calculators.Dark Souls character planner and calculators.Dark Souls character
+                planner and calculators.
+              </p>
+            </div>
+          </div>
+          <div className="footer">
+            <Button className="primary">打开</Button>
+            <Button className="danger">删除</Button>
+          </div>
+        </li>
+        <li>
+          <div className="content">
+            <div className="img-wrapper">
+              <img
+                src="https://lf-cdn-tos.bytescm.com/obj/static/xitu_extension/static/github.46c47564.png"
+                alt="img"
+              />
+            </div>
+            <div>
+              <h2>Souls Planner</h2>
+              <p>
+                Dark Souls character planner and calculators.Dark Souls character planner and
+                calculators.Dark Souls character planner and calculators.Dark Souls character
+                planner and calculators.
+              </p>
+            </div>
+          </div>
+          <div className="footer">
+            <Button className="primary">打开</Button>
+            <Button className="danger">删除</Button>
+          </div>
+        </li>
+        <li>
+          <div className="content">
+            <div className="img-wrapper">
+              <img
+                src="https://lf-cdn-tos.bytescm.com/obj/static/xitu_extension/static/github.46c47564.png"
+                alt="img"
+              />
+            </div>
+            <div>
+              <h2>Souls Planner</h2>
+              <p>
+                Dark Souls character planner and calculators.Dark Souls character planner and
+                calculators.Dark Souls character planner and calculators.Dark Souls character
+                planner and calculators.
+              </p>
+            </div>
+          </div>
+          <div className="footer">
+            <Button className="primary">打开</Button>
+            <Button className="danger">删除</Button>
+          </div>
+        </li>
       </ul>
     </div>
   )
